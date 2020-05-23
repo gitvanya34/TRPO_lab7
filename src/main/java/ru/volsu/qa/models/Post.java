0f7801cc79d52b98759dbf0600380be2c5ed838c
vanya34 <gitvanya34@yandex.ru>
2020-05-24 01:44:03 +0400

    Positive

--- src/main/java/ru/volsu/qa/models/Post.java
+++ src/main/java/ru/volsu/qa/models/Post.java
@@ -1,46 +1,65 @@
 package ru.volsu.qa.models;
 
 public class Post {
-    Integer id;
-    Integer userId;
-    String title;
-    String body;
 
-    public Post(Integer userId, String title, String body) {
-        this.userId = userId;
-        this.title = title;
-        this.body = body;
+   private String first_name;
+   private String last_name;
+   private String gender;
+   private String dob;
+   private String email;
+   private String phone;
+
+
+
+    public Post( String first_name, String last_name, String gender, String dob, String email, String phone) {
+
+         this.first_name=first_name;
+         this.last_name=last_name ;
+         this.gender=gender;
+        this.dob = dob;
+        this.email = email;
+        this.phone = phone;
+
     }
 
-    public Integer getId() {
-        return id;
+
+
+    public String getDob() {
+        return dob;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getEmail() {
+        return email;
     }
 
-    public Integer getUserId() {
-        return userId;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public String getBody() {
-        return body;
-    }
 
-    public void setBody(String body) {
-        this.body = body;
+
+
+
+    public void setDob(String dob) { this.dob = dob;  }
+    public void setEmail(String email) {  this.email = email; }
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getFirst_name() {
+        return first_name;
     }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.gender = last_name;
+    }
+    public String getGender()  {return gender; }
+    public void setGender(String gender) { this.gender = gender; }
 }
--- src/test/java/ru/volsu/qa/ApiTests.java
+++ src/test/java/ru/volsu/qa/ApiTests.java
@@ -1,8 +1,10 @@
 package ru.volsu.qa;
 
+import java.lang.Math;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.volsu.qa.models.Post;
 
@@ -11,82 +13,138 @@ import static org.hamcrest.Matchers.*;
 
 public class ApiTests {
 
+private String token;
+//////Позитивные
     @BeforeClass
     public void beforeClass() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-        RestAssured.port = 443;
+        RestAssured.baseURI = "https://gorest.co.in";
+       RestAssured.port = 443;
+        token="?access-token=oPa_kpfb6A0yhvGdL2p39q7vhgfoTYtWV9d2";
     }
 
-    @Test
-    public void testVerifyStatusCodeForGetPosts() {
-        when()
-                .request("GET", "/posts")
-        .then()
-                .statusCode(200);
-        System.out.println("////////////////////////////////////////////");
+    @DataProvider(name = "testGetUsers_Data")
+    public Object[][] testGetUsers_Data()
+    {
+        return  new Object[][]{
+                {"Jane"},
+                {"Neva"},
+                {"Jonh"}};
     }
+    @DataProvider(name = "testGetUsersUserId_Data")
+    public Object[][] testGetUsersUserId_Data()
+    {
+        return  new Object[][]{
+                {"1451"},
+                {"1452"},
+                {"1453"}};
+    }
+    @DataProvider(name = "testPostUsers_Data")
+    public Object[][] testPostUsers_Data()
+    {
+        return  new Object[][]{
+            {"Salam", "Salam", "female", "1970-01-01", Math.random()+"@example.com", "+1 (903) 946-1126"},
+    //            {"Salam", "Salam", "female", "1970-01-01", "n1b8rr@example.com", "+1 (903) 946-1126"},
+    //            {"Salam", "Salam", "female", "1970-01-01", "n1b8rr@example.com", "+1 (903) 946-1126"}
+        };
+}
 
     @Test
-    public void testVerifyStatusCodeForGetPostsWithLogging() {
+    public void testGetUsers() {
+
         given()
                 .log().all()
         .when()
-                .request("GET", "/posts")
+                .request("GET", "public-api/users"+token)
         .then()
                 .log().all()
                 .statusCode(200);
         System.out.println("////////////////////////////////////////////");
     }
 
-    @Test
-    public void testVerifyStatusCodeForGetPostsWithConditionalLogging() {
+    @Test(dataProvider = "testGetUsers_Data")
+    public void testGetUsersFirstName(String first_name) {
+
         given()
-                .log().ifValidationFails()
+                .log().all()
         .when()
-                .request("GET", "/posts")
+                .request("GET", "/public-api/users"+token+"&first_name="+first_name)
         .then()
-                .log().ifValidationFails()
+                .log().all()
+                .statusCode(200);
+        System.out.println("////////////////////////////////////////////");
+    }
+    @Test(dataProvider = "testGetUsersUserId_Data")
+    public void testGetUsersUserId(String id) {
+
+        given()
+                .log().all()
+        .when()
+                .request("GET", "/public-api/users/"+id+token)
+        .then()
+                .log().all()
                 .statusCode(200);
         System.out.println("////////////////////////////////////////////");
     }
 
-    @Test
-    public void testVerifyResponseTimeForGetPosts() {
-        when()
-                .request("GET", "/posts")
-        .then()
-                .time(lessThan(5000L));
-        System.out.println("////////////////////////////////////////////");
-    }
 
-    @Test
-    public void testVerifyResponseStatusForCreatePost() {
-        Post newPost = new Post(2, "Title", "Some text");
-        given()
+    @Test(dataProvider = "testPostUsers_Data")
+    public void testPostUsers( String first_name, String last_name, String gender, String dob, String email, String phone) {
+        Post newPost = new Post( first_name, last_name,gender, dob, email, phone);
+        given().auth().oauth2("oPa_kpfb6A0yhvGdL2p39q7vhgfoTYtWV9d2")
+                .log().all()
                 .contentType(ContentType.JSON)
                 .body(newPost)
         .when()
-                .post( "/posts")
+                .post( "/public-api/users")
         .then()
-                .statusCode(201);
-        System.out.println("////////////////////////////////////////////");
-    }
-
-    @Test
-    public void testVerifyResponseBodyForCreatePost() {
-        Post newPost = new Post(2, "Title", "Some text");
-        given()
-                .log().ifValidationFails()
-                .contentType(ContentType.JSON)
-                .body(newPost)
-        .when()
-                .post( "/posts")
-        .then()
+                .log().all()
                 .assertThat()
-                .body("userId", equalTo(newPost.getUserId()))
-                .body("title", equalTo(newPost.getTitle()))
-                .body("body", equalTo("Some text"))
-                .body("id", notNullValue());
-        System.out.println("////////////////////////////////////////////");
+                .body("result.first_name", equalTo(newPost.getFirst_name()))
+                .body("result.last_name", equalTo(newPost.getLast_name()))
+                .body("result.gender", equalTo(newPost.getGender()))
+                .body("result.dob", equalTo(newPost.getDob()))
+                .body("result.email", equalTo(newPost.getEmail()))
+                .body("result.phone", equalTo(newPost.getPhone()))
+                 .statusCode(302);
     }
+
+    @Test(dataProvider = "testPostUsers_Data")
+    public void testPutUsersUserId(String first_name, String last_name, String gender, String dob, String email, String phone ) {
+        Post newPost = new Post( first_name, last_name,gender, dob, email, phone);
+        int id =1451;
+        given().auth().oauth2("oPa_kpfb6A0yhvGdL2p39q7vhgfoTYtWV9d2")
+
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(newPost)
+        .when()
+                .put( "/public-api/users/"+id)
+        .then()
+                .log().all()
+                .assertThat()
+                .body("result.first_name", equalTo(newPost.getFirst_name()))
+                .body("result.last_name", equalTo(newPost.getLast_name()))
+                .body("result.gender", equalTo(newPost.getGender()))
+                .body("result.dob", equalTo(newPost.getDob()))
+                .body("result.email", equalTo(newPost.getEmail()))
+                .body("result.phone", equalTo(newPost.getPhone()))
+                 .statusCode(200);
+    }
+
+    @Test()
+    public void testDeleteUsersUserId() {
+        int id =1468;
+        given().auth().oauth2("oPa_kpfb6A0yhvGdL2p39q7vhgfoTYtWV9d2")
+         .log().all()
+         .contentType(ContentType.JSON)
+
+        .when()
+                .delete( "/public-api/users/"+id)
+        .then()
+            .log().all()
+            .body("result", equalTo(null))
+            .statusCode(200);
+    }
+
+
 }
